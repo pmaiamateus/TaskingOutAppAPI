e665c5bf9dcabe7e9d9e047bb1b2fa7cc48de418
mateus.maia <mateus.maia@sistemasdlink.com.br>
2025-05-14 18:45:32 -0300

    Feat: cria controller para update de usuario

---
+++ Controllers/UserControllers/PutUserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskingOutAppAPI.Data;
+using TaskingOutAppAPI.Middlewares;
+using TaskingOutAppAPI.Models;
+
+namespace TaskingOutAppAPI.Controllers.UserControllers;
+
+public class PutUserController : Controller
+{
+    [AllowAnonymous]
+    [HttpPut("user")]
+    public async Task<IActionResult> PutAsync(
+            [FromBody] User UserModel,
+            [FromServices] AppDBContext context)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var UserFromDB = await context.User.FirstOrDefaultAsync(u => u.Email == UserModel.Email);
+                UserFromDB.Name = UserModel.Name;
+                UserFromDB.Password = PasswordHasher.HashPassword(UserModel.Password);
+                context.Update(UserFromDB);
+                await context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest(UserModel);
+
+        }
+        catch (Exception)
+        {
+            return (UnprocessableEntity());
+        }
+    }
+}
