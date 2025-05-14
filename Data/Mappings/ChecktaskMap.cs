7044ff79a37e24de2e4003df251bcc9bb447746d
mateus.maia <mateus.maia@sistemasdlink.com.br>
2025-05-14 01:38:52 -0300

    terminando o dia

---
+++ Data/Mappings/ChecktaskMap.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskingOutAppAPI.Models;
+
+namespace TaskingOutAppAPI.Data.Mappings;
+
+public class ChecktaskMap : IEntityTypeConfiguration<Checktask>
+{
+    public void Configure(EntityTypeBuilder<Checktask> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
