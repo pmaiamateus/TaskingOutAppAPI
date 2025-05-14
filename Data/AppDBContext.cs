125402a597ea05337fc386aa6c09da49382dd779
mateus.maia <mateus.maia@sistemasdlink.com.br>
2025-05-14 01:34:09 -0300

    Feat: criando UserMap para tabela no banco

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using TaskingOutAppAPI.Models;
+using TaskingOutAppAPI.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace TaskingOutAppAPI.Data;
 
 public class AppDBContext : DbContext
 {
-
+    public DbSet<User> User { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlServer("Server=MTS\\SQLEXPRESS;Database=TaskingOutDB;Trust Server Certificate=true;Integrated Security=true;User ID=MTS\\pmaia");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserMap());
+    }
 }
---
+++ Data/Mappings/UserMap.cs
@@ -0,0 +1,50 @@
+﻿using TaskingOutAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TaskingOutAppAPI.Data.Mappings;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("varchar")
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasColumnName("Email")
+            .HasColumnType("varchar")
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Password)
+            .HasColumnName("Password")
+            .HasColumnType("varchar")
+            .HasMaxLength(50);
+
+        builder.Property(x => x.EmailConfirmed)
+            .HasColumnName("EmailConfirmed")
+            .HasColumnType("bit");
+
+        builder.Property(x => x.Role)
+            .HasColumnName("Role")
+            .HasColumnType("varchar")
+            .HasMaxLength(10);
+
+        builder.Property(x => x.Membership)
+            .HasColumnName("Membership")
+            .HasColumnType("varchar")
+            .HasMaxLength(10);
+    }
+}
