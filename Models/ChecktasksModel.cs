5cebe8206ef678ba1e45fc653136cb4f13939d83
mateus.maia <mateus.maia@sistemasdlink.com.br>
2025-05-14 01:17:21 -0300

    Feat: criando model de checktask

---
+++ Models/ChecktasksModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskingOutAppAPI.Models;
+
+public class Checktasks
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = "";
+    public bool IsChecked { get; set; } = false;
+    public bool IsHidden { get; set; } = false;
+}
