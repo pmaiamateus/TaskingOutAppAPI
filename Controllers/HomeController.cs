65221a46bdad516f31e41c380bf52637c17ad463
mateus.maia <mateus.maia@sistemasdlink.com.br>
2025-05-14 17:08:46 -0300

    Feat: cria migrations para criar o DB e cria o home controller para verificar status da API

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskingOutAppAPI.Controllers
+{
+    //HealthCheck route
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
