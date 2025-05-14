2706e2d4017cbebc044457a3097f7e0c7584d921
mateus.maia <mateus.maia@sistemasdlink.com.br>
2025-05-14 02:00:04 -0300

    Update: adiciona linhas em Dbcontext para funcionar as novas tabelas

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -7,10 +7,14 @@ namespace TaskingOutAppAPI.Data;
 public class AppDBContext : DbContext
 {
     public DbSet<User> User { get; set; }
+    public DbSet<Checktask> Checktasks { get; set; }
+    public DbSet<Checklist> Checklists { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     => options.UseSqlServer("Server=MTS\\SQLEXPRESS;Database=TaskingOutDB;Trust Server Certificate=true;Integrated Security=true;User ID=MTS\\pmaia");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UserMap());
+        modelBuilder.ApplyConfiguration(new ChecktaskMap());
+        modelBuilder.ApplyConfiguration(new ChecklistMap());
     }
 }
